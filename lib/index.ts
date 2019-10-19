e95a2481242e4be36f82ffb2aab1543e582b24ac
chenwenhao <cwenhao88888@gmail.com>
2019-10-19 17:33:18 +0800

    安装 typescript

---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+const div: HTMLElement = document.createElement('div')
+div.innerText = "hello world!"
+
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,24 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    merry: "./lib/index.js"
+    merry: "./lib/index.ts"
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
   },
   devServer: {
     contentBase: "./dist"

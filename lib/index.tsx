9081ce64b717e2339e76f783ac83b26078ea117d
chenwenhao <1085810633@qq.com>
2019-10-24 19:00:34 +0800

    完成部分测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -7,34 +7,7 @@ ReactDOM.render(
     <>
         <Icon name='alipay' className='icon'/>
         <Icon name='icon-2'/>
-        <Button onClick={()=>console.log(123123)}>
-            Primary
-        </Button>
-        <Button color={'success'} size={'big'} shape={'rounded'}>
-            Success
-        </Button>
-        <Button color={'danger'}>
-            danger
-        </Button>
-        <Button color={'info'} shape={'circle'} size={'big'} icon={{position: 'left',name: 'alipay'}}>
-            Info
-        </Button>
-        <Button color={'warning'} block outline size={'big'}>
-            Warning
-        </Button>
-        <Button color={'dark'} shape={'circle'} size={'big'} icon={{position: 'right',name: 'alipay'}}>
-            支付宝
-        </Button>
-        <Button color={'danger'} disabled size={'big'}
-                shape={'circle'}
-                icon={{position: 'left',name: 'alipay'}}>
-            danger
-        </Button>
-        <Button color={'gray'} size={'small'}
-                shape={'circle'}
-                >
-            danger
-        </Button>
+        <Button><span>123123</span></Button>
     </>,
     document.getElementById('app'));
 

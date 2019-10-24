cded2a366f9ca43befee647e8d6fbdbfd1933d3e
chenwenhao <cwenhao88888@gmail.com>
2019-10-24 22:49:44 +0800

    更新icon测试

--- test/__tests__/Icon/icon.test.jsx
+++ test/__tests__/Icon/icon.test.jsx
@@ -1,10 +1,18 @@
 import renderer from 'react-test-renderer'
 import Icon from '../../../lib/Icon/icon'
 import React from 'react'
+import {mount} from "enzyme/build/index";
 
 describe('icon',()=>{
   it('是一个svg',()=>{
     const json = renderer.create(<Icon/>).toJSON()
     expect(json).toMatchSnapshot()
+  })
+
+  it('点击icon',()=>{
+    const fn = jest.fn()
+    const c = mount(<Icon onClick={fn}/>)
+    c.find('svg').simulate('click')
+    expect(fn).toBeCalled()
   })
 })

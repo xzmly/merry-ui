5ebddaa529787c995975002c556d800bc50f30fb
chenwenhao <1085810633@qq.com>
2019-10-25 16:56:30 +0800

    add jest-junit

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
 
-  reporters: ["default"],
+  reporters: ["default","jest-junit"],
   globals: {
     "ts-jest": {
       tsConfig: './tsconfig.test.json'

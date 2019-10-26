adcfba0c5d8ebb021aa10c72076e5e2a0ace9840
chenwenhao <cwenhao88888@gmail.com>
2019-10-26 12:25:40 +0800

    导出button和icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,2 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Icon from './Icon/icon';
-import Button from './Button/button';
-
-ReactDOM.render(
-    <>
-        <Icon name='alipay' className='icon'/>
-        <Icon name='icon-2'/>
-        <Button size={'default'} shape={'circle'} icon={{name: 'alipay'}}></Button>
-    </>,
-    document.getElementById('app'));
-
+export { default as Icon } from "./Icon/icon"
+export { default as Button } from "./Button/button"

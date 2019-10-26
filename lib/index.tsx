f88a7a10392a641ae0cbe0368d7e758216038e30
chenwenhao <cwenhao88888@gmail.com>
2019-10-26 17:45:25 +0800

    导出组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,2 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Icon from './components/Icon/icon';
-import Button from './components/Button/button';
-
-ReactDOM.render(
-    <>
-      <Icon name='alipay' className='icon'/>
-      <Icon name='icon-2'/>
-      <Button size={'small'} shape={'rounded'}>更新</Button>
-    </>,
-    document.getElementById('app'));
-
+export { default as Icon } from "./components/Icon/icon"
+export { default as Button } from "./components/Button/button"

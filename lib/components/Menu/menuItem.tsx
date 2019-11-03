dc097884c9b9557f925807729991bdc5d0637bb7
chenwenhao <cwenhao88888@gmail.com>
2019-11-03 14:27:18 +0800

    基础功能完成

--- lib/components/Menu/menuItem.tsx
+++ lib/components/Menu/menuItem.tsx
@@ -5,17 +5,24 @@ import MenuContext from "./context";
 
 export interface MenuItemProps extends React.HTMLAttributes<HTMLElement> {
   name: string
+  itemIndex?: number
 }
+
+const defaultIndex:number = 1;
 
 const MenuItem: React.FC<MenuItemProps> =
     props => {
 
       const { onSelect,selectedName } = useContext(MenuContext)
-      const { className,children,name } = props;
+      const { className,children,name,itemIndex } = props;
+
+      const paddingLeft:number = ((itemIndex || defaultIndex) - 1)*24 + 48;
 
       return (
           <li className={classes(className,'menu-item',selectedName === name ? 'active' : "")}
-              onClick={()=>onSelect && onSelect(name)}>
+              onClick={()=>onSelect && onSelect(name)}
+              style={{paddingLeft: `${paddingLeft}px`}}
+          >
             {children}
           </li>
       )
--- lib/components/Menu/subMenu.tsx
+++ lib/components/Menu/subMenu.tsx
@@ -12,6 +12,7 @@ export interface SubMenuProps {
   className?: string
   children?: ChildrenType | Array<ChildrenType>
   name: string
+  subMenuIndex?: number
 }
 
 const SubMenu: React.FC<SubMenuProps> =
@@ -20,14 +21,15 @@ const SubMenu: React.FC<SubMenuProps> =
       const {
         openNames,
         onSubMenuChange,
-        defaultOpenNames
+        defaultOpenNames,
       } = useContext(MenuContext);
 
       const {
         className,
         children,
         title,
-        name
+        name,
+        subMenuIndex
       } = props;
 
       const defaultNamesOrNames:Array<string> = defaultOpenNames || openNames || [];
@@ -45,16 +47,36 @@ const SubMenu: React.FC<SubMenuProps> =
       const childrenClass = (postfix:string,...names: Array<string>):string =>
           classes("",`sub-menu-${postfix}`,...names);
 
+      const childrens: any =
+          (
+              curArray: any,
+              preArray: any = []
+          ) => {
+            curArray && curArray.forEach((item: any) =>
+              item instanceof Array ?
+                  childrens(item,preArray) :
+                  item.type.name === 'SubMenu' ?
+                      preArray.push(React.cloneElement(item, { subMenuIndex: (subMenuIndex || 1) + 1 })) :
+                  item.type.name == 'MenuItem' ?
+                      preArray.push(React.cloneElement(item, { itemIndex: subMenuIndex || 1 })) :
+                      preArray.push(item)
+            );
+            return preArray
+      };
+
       return (
           <li className={classes(className,'sub-menu',visible ? 'active':'')}>
-            <div className={childrenClass('title')} onClick={() => toggleVisible()}>
+            <div className={childrenClass('title')}
+                 onClick={() => toggleVisible()}
+                 style={{paddingLeft: `${(subMenuIndex || 1)*24}px`}}
+            >
               {title}
               <Icon name={'arrow'}
                     className={childrenClass('arrow')}/>
             </div>
             {visible &&
             <ul className={childrenClass('content')}>
-              {children}
+              {childrens(children)}
             </ul>}
           </li>
       )

b7e05c2366414a0ec20754d43d30041af6d37b08
chenwenhao <cwenhao88888@gmail.com>
2019-11-17 23:24:47 +0800

    init group

---
+++ lib/components/Checkbox/Group.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import classes from '../../helpers/classes';
+
+
+interface GroupProps {
+
+}
+
+const Group: React.FC<GroupProps> =
+        props => {
+
+
+
+  return(
+      <div className={classes('checkbox-group')}>1123</div>
+  )
+};
+
+export default Group

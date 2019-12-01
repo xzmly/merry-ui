9f40bafdbab7eafd034ad82a5c5936a8c30cae91
chenwenhao <cwenhao88888@gmail.com>
2019-12-01 22:00:28 +0800

    创建search

---
+++ lib/components/Input/Search.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Input,{TotalProps}from "./Input";
+import "./Search.styl"
+
+
+const Search: React.FC<TotalProps> = () => {
+  return (<Input>
+
+  </Input>)
+};
+
+export default Search
